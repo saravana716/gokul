9c848536b4a2b2f9be064c7ff31035f568823399
saravana716 <vsaravana716@gmail.com>
2024-02-22 12:27:12 +0530

    first commit

---
+++ src/Pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from "../Component/Navbar"
+const About = () => {
+  return (
+    <>
+    
+    
+    </>
+  )
+}
+
+export default About
---
+++ src/Pages/MyAbout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../Component/Navbar'
+import Footer from '../Component/Footer/Footer'
+import Abouttop from '../Component/Abouttop/Abouttop'
+import Overview from '../Component/Overview/Overview'
+import AboutCompany from '../Component/Aboutcompany/AboutCompany'
+
+const MyAbout = () => {
+  return (
+    <>
+    <Navbar />
+    <Abouttop />
+    <Overview />
+    <AboutCompany />
+    <Footer />
+    </>
+  )
+}
+
+export default MyAbout
